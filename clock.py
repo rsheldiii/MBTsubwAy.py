a45c47c98ff74f22c02779736174d8c2f44a755f
Bob - Home - Windows <rsheldiii@gmail.com>
2016-05-23 20:43:55 -0400

    V2
    current version with multi-threading and live countdown

---
+++ clock.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python
+
+import time, datetime, signal, sys, requests
+from Adafruit_7Segment import SevenSegment
+from threading import Thread, Lock
+
+print "Press CTRL+C to power down"
+
+class Clock:
+  url = "http://realtime.mbta.com/developer/api/v2/predictionsbystop?api_key=wX9NwuHnZU2ToO7GmGR9uw&stop={}&format=json"
+  segment = SevenSegment(address = 0x70)
+  mutex = Lock()
+  time_format = "{:0>2d}{:0>2d}"
+  
+  def __init__(self):
+    self.delay = 7 * 60
+    self.stop_id = 70005
+    self.timestamp = time.time()
+    self.prediction = 0
+    self.exit = False
+    self.delta = 65
+
+  def formatted_url(self):
+    return self.url.format(self.stop_id)
+
+  def formatted_time_string(self):
+    next_prediction = max(self.formatted_time(), 0)
+    return self.time_format.format(next_prediction/60,next_prediction%60)
+
+  # the time into the negatives. used to be used for blinking colon but that's annoying
+  def formatted_time(self):
+    return int(self.prediction - (time.time() - self.timestamp))
+
+  def get_next_prediction(self):
+    next_timestamp = None
+    next_prediction = None
+    
+    try:
+      data = requests.get(self.formatted_url()).json()
+      next_timestamp = int(time.time())
+    except:
+      data = {}
+      print "failed to get data from API"
+
+    if data.get("mode", False):
+      #TODO this might fail if there are no routes, or if a direction is disabled. 
+      raw_trips = data["mode"][0]["route"][0]["direction"][0]["trip"] 
+      predictions = [int(x['pre_away']) for x in raw_trips]
+      predictions.sort # not strictly necessary I dont think
+      filtered_predictions = [x for x in predictions if x > self.delay]
+      if len(filtered_predictions) > 0:
+        next_prediction = filtered_predictions[0]
+
+    self.mutex.acquire()
+    try:
+      if next_timestamp and next_prediction:
+        self.timestamp = next_timestamp
+        self.prediction = next_prediction
+    finally:
+      self.mutex.release()
+
+  def step(self):
+    #TODO "none" shown on clock when no trains?
+    character_string = self.formatted_time_string()
+
+    print self.prediction
+    
+    if int(character_string[0]) != 0:
+      self.segment.writeDigit(0, int(character_string[0])) # Tens
+    else:
+      self.segment.writeDigitRaw(0, 0)
+
+    self.segment.writeDigit(1, int(character_string[1])) # Ones
+    
+    self.handle_colon()
+
+    self.segment.writeDigit(3, int(character_string[2]))   # Tens
+    self.segment.writeDigit(4, int(character_string[3]))        # Ones
+    # Toggle colon
+
+  def handle_colon(self):
+    code = 2 
+    if int(time.time() - self.timestamp) > self.delta:
+      code |= 4
+
+    self.segment.writeDigitRaw(2, code)
+
+  def clock_loop(self):
+    while not self.exit:
+      self.step()
+      time.sleep(0.125)
+
+  def worker_loop(self):
+    while not self.exit: #TODO is it worth looping a bunch in order to check for exit flags
+      self.get_next_prediction()
+      time.sleep(30)
+    self.clear() # so clock clears on exit
+
+  def run(self):
+    worker_thread = Thread(target = self.worker_loop)
+    worker_thread.start()
+    self.clock_loop()
+
+  def exit_gracefully(self, a, b):
+    print("beginning exit...")
+
+    self.mutex.acquire()
+    try:
+      self.exit = True
+    finally:
+      self.mutex.release()
+
+  def clear(self):
+    for i in range(0,5):
+      self.segment.writeDigitRaw(i, 0)
+                     
+c = Clock()
+signal.signal(signal.SIGINT, c.exit_gracefully) # should probably leave out here. don't want each instance setting one for itself
+c.run()
